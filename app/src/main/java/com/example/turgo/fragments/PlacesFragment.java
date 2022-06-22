73a766cdb14bac9000df874e8001c865585e9dd1
Alejandro Malla <amalla2002@fb.com>
2022-06-22 16:40:24 -0700

    direction LatLng on standby

--- app/src/main/java/com/example/turgo/fragments/PlacesFragment.java
+++ app/src/main/java/com/example/turgo/fragments/PlacesFragment.java
@@ -5,9 +5,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.view.menu.MenuView;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -15,7 +13,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -23,34 +20,24 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.example.turgo.MainActivity;
 import com.example.turgo.R;
 import com.google.android.gms.common.api.Status;
-import com.google.android.gms.maps.CameraUpdate;
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
-import com.google.android.libraries.places.api.Places;
 import com.google.android.libraries.places.api.model.Place;
-import com.google.android.libraries.places.api.model.RectangularBounds;
-import com.google.android.libraries.places.api.model.TypeFilter;
 import com.google.android.libraries.places.widget.Autocomplete;
 import com.google.android.libraries.places.widget.AutocompleteActivity;
-import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
-import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
 import com.google.android.libraries.places.widget.model.AutocompleteActivityMode;
 import com.google.gson.Gson;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import java.util.Arrays;
 import java.util.List;
@@ -64,6 +51,7 @@ public class PlacesFragment extends Fragment implements OnMapReadyCallback, Task
     private GoogleMap map;
     private MarkerOptions place1, place2;
     private Polyline currentPolyline;
+    private List<LatLng> directionList;
 
     public PlacesFragment() {    }
     @Override
@@ -85,25 +73,27 @@ public class PlacesFragment extends Fragment implements OnMapReadyCallback, Task
         btnGetDirections = view.findViewById(R.id.btnGetDirections);
         SupportMapFragment mapFragment = (SupportMapFragment) this.getChildFragmentManager().findFragmentById(R.id.mapFragment);
         mapFragment.getMapAsync(this::onMapReady);
-        LatLng position1, position2;
-        position1 = new LatLng(47.606209, -122.332069);
-        position2 = new LatLng(47.620422, -122.349358);
-        place1 = new MarkerOptions().position(position1).title("Location 1");
-        place2 = new MarkerOptions().position(position2).title("Location 2");
-        String url = getUrl(place1.getPosition(), place2.getPosition(), "driving");
+
+
+
+
+
 
         btnGetDirections.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-
-                // Direction API
-
+                // make link for directions
+                LatLng position1, position2;
+                position1 = new LatLng(47.606209, -122.332069);
+                position2 = new LatLng(47.620422, -122.349358);
+                // mark place
+                place1 = new MarkerOptions().position(position1).title("Origin");
+                place2 = new MarkerOptions().position(position2).title("Destination");
+                String DIRECTION_URL = getUrl(place1.getPosition(), place2.getPosition(), "steps");
                 // Instantiate the RequestQueue.
                 RequestQueue queue = Volley.newRequestQueue(getActivity());
-                String origin = "Disneyland", destination = "Universal+Studios+Hollywood", google_api_key = getString(R.string.google_api_key);
-                String DIRECTION_URL = "https://maps.googleapis.com/maps/api/directions/json?origin="+origin+"&destination="+destination+"&key=" + google_api_key;
-        //         Request a string response from the provided URL.
+                // Request a string response from the provided URL.
                 StringRequest stringRequest = new StringRequest(Request.Method.GET, DIRECTION_URL,
                         new Response.Listener<String>() {
                             @Override
@@ -111,16 +101,14 @@ public class PlacesFragment extends Fragment implements OnMapReadyCallback, Task
                                 // Display the first 500 characters of the response string.
                                 btnGetDirections.setText("Response is: " + response);
         //                        tvJsonResponse.setText(response.get);
-                                Gson g = new Gson();
-                                JSONArray array = null;
-                                try {
-                                    array = new JSONArray(response);
-                                    Log.i(TAG, array.getJSONObject(0).toString());
-                                } catch (JSONException e) {
-                                    e.printStackTrace();
-                                }
-
-
+                                Gson gson = new Gson();
+                                JsonObject jsonObject = new JsonParser().parse(response).getAsJsonObject();
+                                JsonArray routes = jsonObject.getAsJsonArray("routes");
+                                Log.i(TAG, routes.toString());
+                                JsonElement narrow = routes.get(0);
+                                Log.i(TAG, narrow.toString());
+                                JsonArray legs = ((JsonObject) narrow).getAsJsonArray("legs");
+                                Log.i(TAG, legs.toString());
                             }
                         }, new Response.ErrorListener() {
                     @Override
@@ -129,6 +117,7 @@ public class PlacesFragment extends Fragment implements OnMapReadyCallback, Task
                     }
                 });
                 // Add the request to the RequestQueue.
+
                 queue.add(stringRequest);
             }
         });
@@ -164,15 +153,21 @@ public class PlacesFragment extends Fragment implements OnMapReadyCallback, Task
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
         map = googleMap;
-        map.addMarker(place1);
-        map.addMarker(place2);
-        LatLngBounds.Builder builder = new LatLngBounds.Builder();
-        builder.include(place1.getPosition()); builder.include(place2.getPosition());
-        LatLngBounds bounds = builder.build();
-        int padding = 100;
-        CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, padding);
-//        googleMap.moveCamera(cu);
-        googleMap.animateCamera(cu);
+//        map.addMarker(place1);
+//        map.addMarker(place2);
+//        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+//        builder.include(place1.getPosition()); builder.include(place2.getPosition());
+//        LatLngBounds bounds = builder.build();
+//        int padding = 100;
+//        CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, padding);
+////        googleMap.moveCamera(cu);
+//        googleMap.animateCamera(cu);
+//
+//        Polyline polyline1 = googleMap.addPolyline(new PolylineOptions()
+//                        .clickable(true)
+//                        .addAll(directionList));
+////        map.addPolyline(PolylineOptions )
+
     }
 
     private String getUrl(LatLng origin, LatLng dest, String directionMode) {

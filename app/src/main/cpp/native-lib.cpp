c09ced73d901697e7a91fef63426c88ea8ca0cf1
Alejandro Malla <amalla2002@fb.com>
2022-07-22 14:15:27 -0700

    Added comments

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -11,22 +11,26 @@ extern "C"
 JNIEXPORT jintArray JNICALL
 Java_com_example_turgo_fragments_ParksFragment_queueTree(JNIEnv *env, jobject thiz, jintArray tree,
                                                          jint target) {
+    // moving things into normal cpp types
     jsize size = env->GetArrayLength( tree );
     vector<int>T( size ), Ans;
     env->GetIntArrayRegion( tree, jsize{0}, size, &T[0] );
+
+    // normal BFS
     queue<int> q;
     int x = target;
     q.push(1);
     while (!q.empty()) {
         int v = q.front(); q.pop();
         if (T[v]>x) continue;
-        if (v>=(T.size()-24)/2) {
-            cout << v << " "  << v-(T.size()-24)/2 << " " << T[v] << "\n";
+        if (v>=(T.size()-24)/2) { // is in last row of tree (data)
             Ans.push_back(v-(T.size()-24)/2);
             continue;
         }
-        q.push(v*2); q.push(v*2+1);
+        q.push(v*2); q.push(v*2+1); // adds the current nodes' childs
     }
+
+    //converts answers back to java types
     jintArray ans = env->NewIntArray(Ans.size());
     env->SetIntArrayRegion(ans, jsize{0}, env->GetArrayLength(ans),&Ans[0]);
     return ans;
@@ -35,13 +39,20 @@ extern "C"
 JNIEXPORT jintArray JNICALL
 Java_com_example_turgo_MainActivity_buildTree(JNIEnv *env, jobject thiz, jintArray people) {
     const int INF = 1e7;
+    // move things to normal cpp types
     jsize size = env->GetArrayLength( people );
     vector<int> A(size);
     env->GetIntArrayRegion( people, jsize{0}, size, &A[0] ); // fills A with people
+
+    // looks for a power of 2 for the size of tree
     int pow2 = 1; for (int i = 1; pow2<A.size(); ++i) pow2*=2;
+
+    // build tree
     vector<int> segTree(pow2*2+24, INF);
     for (int i = 0, k = pow2; i<A.size(); ++i, ++k) segTree[k] = 0;
     for (int i = pow2-1; i>0; --i) segTree[i] = min(segTree[i*2], segTree[i*2+1]);
+
+    // moves answers back to java types
     jintArray ans = env->NewIntArray(segTree.size());
     env->SetIntArrayRegion(ans, jsize{0}, env->GetArrayLength(ans),&segTree[0]);
     return ans;
@@ -50,13 +61,20 @@ extern "C"
 JNIEXPORT jintArray JNICALL
 Java_com_example_turgo_fragments_VisitParkFragment_updateTree(JNIEnv *env, jobject thiz,
                                                               jintArray tree, jint pos, jint val) {
+    // move things to normal cpp types
     jsize size = env->GetArrayLength( tree );
     vector<int> T(size);
     env->GetIntArrayRegion(tree, jsize{0}, size, &T[0]);
+
+    // get position of data in tree
     pos += (T.size()-24)/2;
     T[pos] += val; // not using T[pos] = val in case there are multiple updates at the same time
+
+    // start at the parent and go to the root recalculating min
     pos /= 2;
     for (; pos>0; pos/=2) T[pos] = min(T[2*pos], T[2*pos+1]);
+
+    // move things back to java types
     env->SetIntArrayRegion(tree, jsize{0}, size, &T[0]);
     return tree;
 }
@@ -64,6 +82,7 @@ extern "C"
 JNIEXPORT jintArray JNICALL
 Java_com_example_turgo_fragments_PlacesFragment_updateTree(JNIEnv *env, jobject thiz,
                                                               jintArray tree, jint pos, jint val) {
+    // see above function
     jsize size = env->GetArrayLength( tree );
     vector<int> T(size);
     env->GetIntArrayRegion(tree, jsize{0}, size, &T[0]);
@@ -81,6 +100,7 @@ Java_com_example_turgo_fragments_FlightsFragment_findBestCombination(JNIEnv *env
                                                                      jdoubleArray going_price_flights, jdoubleArray returning_price_flights, jdoubleArray hotel_price,
                                                                      jintArray going_indices, jintArray returning_indices,
                                                                      jint min_days, jint max_days) {
+    // move things to normal cpp types and prepare data containers
     jsize size = env->GetArrayLength( going_price_flights );
     jsize size1 = env->GetArrayLength( going_indices );
     int minDays = min_days, maxDays = max_days;
@@ -91,13 +111,17 @@ Java_com_example_turgo_fragments_FlightsFragment_findBestCombination(JNIEnv *env
     env->GetDoubleArrayRegion(hotel_price, jsize{0}, size, &stay[0]);
     env->GetIntArrayRegion(going_indices, jsize{0}, size1, &starts[0]);
     env->GetIntArrayRegion(returning_indices, jsize{0}, size1, &ends[0]);
+
+    // get sums from 0 to ith for all i
     dp[starts[0]-1] = 0;
     for (int i = starts[0]; i<=ends[1]; ++i) dp[i] = dp[i-1] + stay[i-1];
+
+    // check starts with ends and add costs
     for (int i = starts[0]; i<=starts[1]; ++i) {
         for (int j = ends[0]; j<=ends[1]; ++j) {
-            if (i>=j) continue;
-            if (!(j-(i+1)>=minDays && j-(i+1)<=maxDays)) continue;
-            double sum = ori[i]+dest[j]+(dp[j]-dp[i]); // Assuming hotel is in USD
+            if (i>=j) continue; // arrive before depart
+            if (!(j-(i+1)>=minDays && j-(i+1)<=maxDays)) continue; // between day range
+            double sum = ori[i]+dest[j]+(dp[j]-dp[i]); // Assuming hotel price is in USD
             if (sum<answer[0]) {
                 answer[0] = sum;
                 answer[1] = i;
@@ -110,6 +134,8 @@ Java_com_example_turgo_fragments_FlightsFragment_findBestCombination(JNIEnv *env
             }
         }
     }
+
+    // move things back to java
     jdoubleArray ans = env->NewDoubleArray(answer.size());
     env->SetDoubleArrayRegion(ans, jsize{0}, env->GetArrayLength(ans), &answer[0]);
     return ans;
